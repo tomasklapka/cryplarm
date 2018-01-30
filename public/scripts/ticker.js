59e35cac71ecbbc7e8190e6c3665bcd5c3e8f84d
Tomáš Klapka <tomas@klapka.cz>
2018-01-30 19:52:44 +0100

    add checkbox for live updates

--- public/scripts/ticker.js
+++ public/scripts/ticker.js
@@ -1,20 +1,23 @@
 
 function ticker() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      const js = JSON.parse(this.responseText);
-      document.getElementById("last_price").innerHTML = js.last_price;
-      document.getElementById("alarms_list_up").innerHTML = js.alarms_list_up;
-      document.getElementById("alarms_list_down").innerHTML = js.alarms_list_down;
-      const field = document.getElementById("alarmPrice");
-      if (!field.value || !field.value > 0) {
-        field.value = js.last_price;
+  const ticking = document.getElementById("liveMode").checked;
+  if (ticking) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        const js = JSON.parse(this.responseText);
+        document.getElementById("last_price").innerHTML = js.last_price;
+        document.getElementById("alarms_list_up").innerHTML = js.alarms_list_up;
+        document.getElementById("alarms_list_down").innerHTML = js.alarms_list_down;
+        const field = document.getElementById("alarmPrice");
+        if (!field.value || !field.value > 0) {
+          field.value = js.last_price;
+        }
       }
-    }
-  };
-  xhttp.open("GET", "/ticker", true);
-  xhttp.send();
+    };
+    xhttp.open("GET", "/ticker", true);
+    xhttp.send();
+  }
 }
 
 function start_ticking() {
